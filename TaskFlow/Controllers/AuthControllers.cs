1e40a6fea4f93acf0048044728d6185e535ecfaa
Leonardo Loiacono <l.loiacono@serandp.com>
2026-05-12 12:06:11 +0200

    feat: add authentication endpoints

---
+++ TaskFlow/Controllers/AuthControllers.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskFlow.DTOs.Auth;
+using TaskFlow.Interfaces.Services;
+
+namespace TaskFlow.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthControllers : ControllerBase
+{
+    private readonly IAuthServices _authServices;
+    
+    public AuthControllers(IAuthServices authServices)
+    {
+        _authServices = authServices;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<AuthResponse>> Register(RegisterRequest request)
+    {
+        var response = await _authServices.RegisterAsync(request);
+        return Ok(response);
+    }
+    
+    [HttpPost("login")]
+    public async Task<ActionResult<AuthResponse>> Login(LoginRequest request)
+    {
+        var response = await _authServices.LoginAsync(request);
+        return Ok(response);
+    }
+}
