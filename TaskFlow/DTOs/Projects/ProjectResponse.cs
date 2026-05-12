d5636b3c3e0a3e4ec1e9aeafdcd29600f290f597
Leonardo Loiacono <l.loiacono@serandp.com>
2026-05-12 15:39:28 +0200

    feat: add project DTOs

---
+++ TaskFlow/DTOs/Projects/ProjectResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskFlow.DTOs.Projects;
+
+public class ProjectResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Guid OwnerId { get; set; }
+    public int TaskCount { get; set; }
+}
